4f44d87d9b4e76e8fcf036e4744ced95c9f22261
Shane Booth <shane.booth@student.manchester.ac.uk>
2015-07-02 10:28:23 +0100

    Cleanup of ooo stuff

--- io/darkcraft/procsim/model/simulator/OoOIssueSimulator.java
+++ io/darkcraft/procsim/model/simulator/OoOIssueSimulator.java
@@ -15,8 +15,8 @@ import java.util.List;
 
 public class OoOIssueSimulator extends OoOExSimulator
 {
-	private static final int maxSize = 16;
-	private List<IInstruction> window = new ArrayList(maxSize);
+	private static final int	maxSize	= 16;
+	private List<IInstruction>	window	= new ArrayList(maxSize);
 
 	public OoOIssueSimulator(IMemory _mem, IRegisterBank _reg, InstructionReader _reader, AbstractPipeline... _pipelines)
 	{
@@ -26,22 +26,23 @@ public class OoOIssueSimulator extends OoOExSimulator
 	@Override
 	public void flushInstructionCache(int id)
 	{
-		if(window.isEmpty()) return;
-		for(Iterator<IInstruction> iter = window.iterator(); iter.hasNext();)
+		if (window.isEmpty())
+			return;
+		for (Iterator<IInstruction> iter = window.iterator(); iter.hasNext();)
 		{
 			IInstruction i = iter.next();
-			if(i.getAddress() > id)
+			if (i.getAddress() > id)
 				iter.remove();
 		}
 	}
 
 	private void nextInstructionsToWindow()
 	{
-		for(int i = 0; i < pipeline.length*2 && window.size() < maxSize; i++)
+		for (int i = 0; i < pipeline.length * 2 && window.size() < maxSize; i++)
 		{
 			int pc = reg.getValue("PC", null);
 			IInstruction inst = reader.get(pc);
-			if(inst != null)
+			if (inst != null)
 			{
 				window.add(inst);
 				reg.incrementPC();
@@ -51,60 +52,39 @@ public class OoOIssueSimulator extends OoOExSimulator
 
 	private void assignFromWindow()
 	{
-		mainLoop:
-		for(int counter = 0; counter < pipeline.length; counter++)
+		mainLoop: for (int counter = 0; counter < pipeline.length; counter++)
 		{
-			instructionLoop:
-			for(int i = 0; i < window.size(); i++)
+			instructionLoop: for (int i = 0; i < window.size(); i++)
 			{
 				IInstruction inst = window.get(i);
-				//Check it doesn't conflict with anything earlier in the list
-				for(int j = 0; j < i; j++)
+				// Check it doesn't conflict with anything earlier in the list
+				for (int j = 0; j < i; j++)
 				{
 					IInstruction to = window.get(j);
-					if(to instanceof Branch || (to != null &&
-												to.getOutputRegister() != null && to.getOutputRegister().equals("PC")))
-						continue instructionLoop;
+					if (to instanceof Branch || (to != null && to.getOutputRegister() != null && to.getOutputRegister().equals("PC")))
+						return;
 					List<IDependency> deps = DependencyGraphBuilder.getDependencies(inst, to);
-					if(deps.size() > 0)
+					if (deps.size() > 0)
 						continue instructionLoop;
 				}
 
-				//Check pipeline stages which haven't had registers locked
-				for(AbstractPipeline pl : pipeline)
-					for(int j = 0; j <= pl.getLastIDStage(); j++)
+				// Check pipeline stages which haven't had registers locked
+				for (AbstractPipeline pl : pipeline)
+					for (int j = 0; j <= pl.getLastIDStage(); j++)
 					{
 						IInstruction to = pl.getInstruction(j);
-						if(to == null) continue;
+						if (to == null)
+							continue;
 						List<IDependency> deps = DependencyGraphBuilder.getDependencies(inst, to);
-						if(deps.size() > 0)
+						if (deps.size() > 0)
 							continue instructionLoop;
 					}
 
-				//Ok, check what registers are locked
-				/*{
-					String outReg = inst.getOutputRegister();
-					if(outReg != null && !outReg.isEmpty())
+				// Try to add the instruction to a pipeline
+				for (AbstractPipeline pl : pipeline)
+					if (pl.addInstruction(inst))
 					{
-						Register r = reg.get(outReg);
-						if(r.isLocked()) continue instructionLoop;
-					}
-					String[] inRegs = inst.getInputRegisters();
-					if(inRegs != null && inRegs.length > 0)
-					{
-						for(String inReg : inRegs)
-						{
-							if(inReg == null) continue;
-							Register r = reg.get(inReg);
-							if(r.isLocked()) continue instructionLoop;
-						}
-					}
-				}*/
-
-				for(AbstractPipeline pl : pipeline)
-					if(pl.addInstruction(inst))
-					{
-						for(AbstractPipeline pl2 : pipeline)
+						for (AbstractPipeline pl2 : pipeline)
 							pl2.increaseTimer();
 						window.remove(inst);
 						continue mainLoop;
